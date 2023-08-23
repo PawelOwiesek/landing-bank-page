50587e02f58e6f0c6863fd6f108201a00dbb12da
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-23 07:53:25 +0200

    Add SubSection with styles and images

--- src/core/App.js
+++ src/core/App.js
@@ -2,6 +2,7 @@ import Header from "../common/Header";
 import MainSection from "../common/MainSection";
 import Section from "../common/Section";
 import Main from "../features/Main";
+import SubSection from "../features/SubSection";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <MainSection />
       <Section title="Why choose Easybank?" body={<Main />} />
+      <Section title="Latest Articles" body={<SubSection />} />
     </>
   );
 }
---
+++ src/features/SubSection/index.js
@@ -0,0 +1,41 @@
+import Tile from "../../common/Tile";
+import { Wrapper } from "../Main/styled";
+import { Image } from "./styled";
+import money from "./images/image-currency.jpg";
+import restaurant from "./images/image-restaurant.jpg";
+import plane from "./images/image-plane.jpg";
+import confetti from "./images/image-confetti.jpg";
+
+const SubSection = () => {
+  return (
+    <>
+      <Wrapper>
+        <Tile
+          icon={<Image src={money} alt="money" />}
+          subTitle="By Claire Robinson"
+          title="Receive money in any currency with no fees "
+          content="The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single … "
+        />
+        <Tile
+          icon={<Image src={restaurant} alt="restaurant" />}
+          subTitle="By Wilson Hutton"
+          title=" Treat yourself without worrying about money"
+          content="Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …"
+        />
+        <Tile
+          icon={<Image src={plane} rel="plane" />}
+          subTitle="By Wilson Hutton"
+          title="Take your Easybank card wherever you go"
+          content="We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …"
+        />
+        <Tile
+          icon={<Image src={confetti} rel="confetti" />}
+          subTitle="By Claire Robinson"
+          title="Our invite-only Beta accounts are now live!"
+          content="After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ... "
+        />
+      </Wrapper>
+    </>
+  );
+};
+export default SubSection;
