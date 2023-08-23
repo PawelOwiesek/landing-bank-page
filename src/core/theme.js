ce61f9c42a514ec0a3f4c5d4e0aedf648c86208f
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-23 06:43:58 +0200

    add theme styles

---
+++ src/core/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  color: {
+    DarkBlue: "hsl(233, 26%, 24%)",
+    LimeGreen: "hsl(136, 65%, 51%)",
+    BrightCyan: "hsl(192, 70%, 51%)",
+    GrayishBlue: "#9698a6",
+    LightGrayishBlue: "hsl(220, 16%, 96%)",
+    VeryLightGray: "hsl(0, 0%, 98%)",
+    White: "hsl(0, 0%, 100%)",
+  },
+
+  breakpoints: {
+    mobile: "375px",
+    desktop: "1440px",
+  },
+};
