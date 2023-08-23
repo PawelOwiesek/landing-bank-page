3a9f168bb2be4ff36bd8d1a75e18a8790b56fc68
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-23 07:44:55 +0200

    imported Section to App

--- src/core/App.js
+++ src/core/App.js
@@ -1,11 +1,14 @@
 import Header from "../common/Header";
 import MainSection from "../common/MainSection";
+import Section from "../common/Section";
+import Main from "../features/Main";
 
 function App() {
   return (
     <>
       <Header />
       <MainSection />
+      <Section title="Why choose Easybank?" body={<Main />} />
     </>
   );
 }
