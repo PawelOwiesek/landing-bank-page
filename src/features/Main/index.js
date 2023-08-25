097b61186a9a28a88bd34f796caa80fbdbd8b720
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-25 11:46:30 +0200

    content from direct text to  data from object.

--- src/features/Main/index.js
+++ src/features/Main/index.js
@@ -1,4 +1,5 @@
 import Tile from "../../common/Tile";
+import { features } from "../../assets/data";
 import {
   ApiIcon,
   BudgetIcon,
@@ -11,35 +12,27 @@ import {
 const Main = () => {
   return (
     <>
-      <Info>
-        We leverage Open Banking to turn your bank account into your financial
-        hub.
-        <br /> Control your finances like never before.
-      </Info>
+      <Info>{features.info.content}</Info>
       <Wrapper>
         <Tile
           icon={<OnlineIcon />}
-          title="Online Banking"
-          content="Our modern web and mobile applications allow you to keep track of
-              your finances wherever you are in the world."
+          title={features.online.title}
+          content={features.online.content}
         />
         <Tile
           icon={<BudgetIcon />}
-          title="Simple Budgeting"
-          content="See exactly where your money goes each month. Receive
-              notifications when you’re close to hitting your limits"
+          title={features.budget.title}
+          content={features.budget.content}
         />
         <Tile
           icon={<OnBoardingIcon />}
-          title="Fast Onboarding"
-          content="We don’t do branches. Open your account in minutes online and
-              start taking control of your finances right away."
+          title={features.onBoarding.title}
+          content={features.onBoarding.content}
         />
         <Tile
           icon={<ApiIcon />}
-          title="Open API"
-          content="Manage your savings, investments, pension, and much more from one
-              account. Tracking your money has never been easier."
+          title={features.api.title}
+          content={features.api.content}
         />
       </Wrapper>
     </>
--- src/features/SubSection/index.js
+++ src/features/SubSection/index.js
@@ -1,4 +1,5 @@
 import Tile from "../../common/Tile";
+import { articles } from "../../assets/data";
 import { Wrapper } from "../Main/styled";
 import { Image } from "./styled";
 import money from "./images/image-currency.jpg";
@@ -12,27 +13,27 @@ const SubSection = () => {
       <Wrapper id="Blog">
         <Tile
           icon={<Image src={money} alt="money" />}
-          subTitle="By Claire Robinson"
-          title="Receive money in any currency with no fees "
-          content="The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single … "
+          subTitle={articles.blog.subTitle}
+          title={articles.blog.title}
+          content={articles.blog.content}
         />
         <Tile
           icon={<Image src={restaurant} alt="restaurant" />}
-          subTitle="By Wilson Hutton"
-          title=" Treat yourself without worrying about money"
-          content="Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …"
+          subTitle={articles.restaurant.subTitle}
+          title={articles.restaurant.title}
+          content={articles.restaurant.content}
         />
         <Tile
           icon={<Image src={plane} alt="plane" />}
-          subTitle="By Wilson Hutton"
-          title="Take your Easybank card wherever you go"
-          content="We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …"
+          subTitle={articles.plane.subTitle}
+          title={articles.plane.title}
+          content={articles.plane.content}
         />
         <Tile
           icon={<Image src={confetti} alt="confetti" />}
-          subTitle="By Claire Robinson"
-          title="Our invite-only Beta accounts are now live!"
-          content="After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ... "
+          subTitle={articles.confetti.subTitle}
+          title={articles.confetti.title}
+          content={articles.confetti.content}
         />
       </Wrapper>
     </>
